b076ea4fc71a564bdae3d76eb725eb0f08907e37
Nick Walker <nick@nickwalker.us>
2015-05-09 22:05:40 -0500

    Add package boilerplate

---
+++ README.rst
@@ -0,0 +1,12 @@
+PG-ELLA
+======
+
+.. image:: https://travis-ci.org/EllaBot/PG-ELLA.svg
+
+A python implementation of PG-ELLA, an algorithm for efficient multi-task learning.[1][2]
+
+References
+----------
+
+[1] Ammar, Eaton, E.; Ruvolo, P.; and Taylor, M.. "Online multi-task learning for policy gradient methods". Proceedings of the 31st International Conference on Machine Learning. 2014.
+[2] Ruvolo, P., and Eaton, E.. "ELLA: An efficient lifelong learning algorithm". Journal of Machine Learning Research. 2013.
---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import setup
+
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+
+setup(
+    name='pg_ella',
+    version='0.0.1',
+    description='A python implementation of PG-ELLA, an algorithm for efficient multi-task learning',
+    long_description=(read('README.rst')),
+    url='https://github.com/ellabot/PG-ELLA/',
+    license='MIT',
+    author='Rishi Shah, Nick Walker',
+    py_modules=['pg_ella'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=['nose', 'numpy', 'matplotlib']
+)
